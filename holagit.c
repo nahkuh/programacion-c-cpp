02f9d2c3e7ac3eda728537f10bb905c923a4dfd0
nahkuh <nofai.aguilera@gmail.com>
2025-09-10 19:13:05 -0600

    Este es mi tercer commit que tanto trabajo costó, tuve que cerrar vscode

--- holagit.c
+++ holagit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(){
-    printf("Hola desde C git =)\n");
+    printf("Hola desde C git =) \n");
+    printf("hola git, espero poder hacer el 3er commit\n");//esta es la linea que añadí para test git
     return 0;
 }
