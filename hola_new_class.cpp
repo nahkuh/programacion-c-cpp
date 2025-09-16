511939c6cec9dec08887e8daa4544cfbae8277e0
nahkuh <nofai.aguilera@gmail.com>
2025-09-16 11:19:48 -0600

    Git 3 login V3 hi new

--- hola_new_class.cpp
+++ hola_new_class.cpp
@@ -6,5 +6,6 @@ int main(){
     std::cout << "Hola mundo de C++";
     std::cout << "Hola git, vamos a add y commitarte \n";
     std::cout << "hola clase 2\n";
+    std::cout << "V3 de hola new";
     return 0;
 }
