0ef9b82a1f0a6a63e99c152b1bd0eb7c6fe07be5
nahkuh <nofai.aguilera@gmail.com>
2025-09-16 12:49:35 -0600

    guardamos los cambios que teníamos en stash

--- login.c
+++ login.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("Hola login, empiza la nueva rama branch login.\n");
+    printf("Hola login, empiza la nueva rama branch login.\nV2 login");
     /* code */
     return 0;
 }
