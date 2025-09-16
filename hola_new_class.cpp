49cd9d75cc1bc0dd7d96b52c42cf82b08ef1be11
nahkuh <nofai.aguilera@gmail.com>
2025-09-16 03:00:14 -0600

    "Aqui empieza la clase 2"

---
+++ hola_new_class.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    std::cout << "Hola mundo de C++";
+    std::cout << "Hola git, vamos a add y commitarte \n";
+    std::cout << "hola clase 2\n";
+    return 0;
+}
