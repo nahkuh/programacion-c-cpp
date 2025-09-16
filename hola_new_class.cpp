143e70326bb17aa23223c71a0ef94f506284fa46
nahkuh <nofai.aguilera@gmail.com>
2025-09-16 12:49:35 -0600

    guardamos los cambios que teníamos en stash

--- hola_new_class.cpp
+++ hola_new_class.cpp
@@ -6,8 +6,10 @@ int main(){
     std::cout << "Hola mundo de C++";
     std::cout << "Hola git, vamos a add y commitarte \n";
 
+
     std::cout << "hola clase 2\n";
     std::cout << "V3 de hola new";
+
 
     return 0;
 }
