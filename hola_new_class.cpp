781d6d1a6759509ba787936e4703d93f55e217da
nahkuh <nofai.aguilera@gmail.com>
2025-09-21 18:52:05 -0600

    resuelve conflictos con archivo c++

--- hola_new_class.cpp
+++ hola_new_class.cpp
@@ -5,7 +5,9 @@ int main(){
 
     std::cout << "Hola mundo de C++";
     std::cout << "Hola git, vamos a add y commitarte \n";
+
     std::cout << "hola clase 2\n";
     std::cout << "V3 de hola new";
+
     return 0;
 }
